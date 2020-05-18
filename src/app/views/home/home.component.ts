860800b29cc9961b00eac0cdf7cc91a4c011a5d4
Leo Kiszewski <kiszewski1999@gmail.com>
2020-05-18 19:49:19 -0300

    adicionado alteração no header conforme a pagina

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HeaderService } from './../../components/template/header/header.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private headerService: HeaderService) {
+    this.headerService.headerData = {
+      title: 'Início',
+      icon: 'home',
+      url: ''
+    }
+  }
 
   ngOnInit(): void {
   }
-
 }
