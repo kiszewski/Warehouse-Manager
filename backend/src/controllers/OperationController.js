b71df4798e9c25f8963e1dd211be9e5d82f07b8f
Leo Kiszewski <kiszewski1999@gmail.com>
2020-06-08 20:26:22 -0300

    alterado formato de data enviada para frontend

--- backend/src/controllers/OperationController.js
+++ backend/src/controllers/OperationController.js
@@ -2,20 +2,29 @@ const knex = require('../database')
 
 module.exports = {
     async index(req, res, next) {
-        // let result = await knex('products_warehouses')
-        //     .innerJoin('products', 'products_warehouses.product_id', 'products.id')
-        //     // .innerJoin('warehouses', 'prodcuts_warehouses.warehouse_id', 'warehouse.id')
+        try {
+            let result = await knex('products_warehouses')
+                .select('products_warehouses.ns',
+                    'products_warehouses.created_at as date',
+                    'products.name as product_name',
+                    'warehouses.name as warehouse_name')
+                .from('products')
+                .joinRaw(`inner join products_warehouses on products_warehouses.product_id = products.id 
+                inner join warehouses on products_warehouses.warehouse_id = warehouses.id`)
 
-        let result = await knex('products_warehouses')
-            .select('products_warehouses.ns', 'products_warehouses.created_at as date', 'products.name as product_name', 'warehouses.name as warehouse_name')
-            .from('products')
-            .joinRaw('inner join products_warehouses on products_warehouses.product_id = products.id inner join warehouses on products_warehouses.warehouse_id = warehouses.id')
+            var options = { year: 'numeric', month: 'numeric', day: 'numeric' };
 
+            result.forEach(operation => {
+                operation.date = operation.date.toLocaleDateString('en-US', options);
+            })
 
-        res.json(result)
+            res.json(result)
+        } catch (error) {
+            next(error)
+        }
     },
 
     async indexById() {
-        
+
     }
 }
