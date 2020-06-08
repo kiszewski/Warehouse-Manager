197b7c8f363a481a1e5a01919b329f21ce383976
Leo Kiszewski <kiszewski1999@gmail.com>
2020-06-08 20:47:41 -0300

    criado metodo de pegar movimentação por NS

--- backend/src/controllers/OperationController.js
+++ backend/src/controllers/OperationController.js
@@ -3,16 +3,30 @@ const knex = require('../database')
 module.exports = {
     async index(req, res, next) {
         try {
-            let result = await knex('products_warehouses')
-                .select('products_warehouses.ns',
-                    'products_warehouses.created_at as date',
-                    'products.name as product_name',
-                    'warehouses.name as warehouse_name')
-                .from('products')
-                .joinRaw(`inner join products_warehouses on products_warehouses.product_id = products.id 
-                inner join warehouses on products_warehouses.warehouse_id = warehouses.id`)
+            var options = { year: 'numeric', month: 'numeric', day: 'numeric' }
 
-            var options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+            if (Object.keys(req.query).length === 0) {
+                result = await knex('products_warehouses')
+                    .select('products_warehouses.ns',
+                        'products_warehouses.created_at as date',
+                        'products.name as product_name',
+                        'warehouses.name as warehouse_name')
+                    .from('products')
+                    .joinRaw(`inner join products_warehouses on products_warehouses.product_id = products.id 
+                    inner join warehouses on products_warehouses.warehouse_id = warehouses.id`)
+            } else {
+                const { ns } = req.query
+
+                result = await knex('products_warehouses')
+                    .select('products_warehouses.ns',
+                        'products_warehouses.created_at as date',
+                        'products.name as product_name',
+                        'warehouses.name as warehouse_name')
+                    .from('products')
+                    .joinRaw(`inner join products_warehouses on products_warehouses.product_id = products.id 
+                inner join warehouses on products_warehouses.warehouse_id = warehouses.id`)
+                    .where({ ns })
+            }
 
             result.forEach(operation => {
                 operation.date = operation.date.toLocaleDateString('en-US', options);
@@ -22,9 +36,5 @@ module.exports = {
         } catch (error) {
             next(error)
         }
-    },
-
-    async indexById() {
-
     }
 }
